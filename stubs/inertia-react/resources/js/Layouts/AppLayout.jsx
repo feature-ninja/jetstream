b76c761350f41d5cfa190bab4c0e89c0d627ee10
Roj Vroemen <me@rojvroemen.com>
2021-03-18 13:47:00 +0100

    Fixing conditional classnames

--- stubs/inertia-react/resources/js/Layouts/AppLayout.jsx
+++ stubs/inertia-react/resources/js/Layouts/AppLayout.jsx
@@ -7,6 +7,7 @@ import JetNavLink from '@/Jetstream/NavLink';
 import JetResponsiveNavLink from '@/Jetstream/ResponsiveNavLink';
 import { Inertia } from '@inertiajs/inertia';
 import { InertiaLink, usePage } from '@inertiajs/inertia-react';
+import classNames from 'classnames';
 
 function switchToTeam(team) {
     Inertia.put(route('current-team.update'), {
@@ -156,8 +157,8 @@ export default function AppLayout({ header, children}) {
                             <div className="-mr-2 flex items-center sm:hidden">
                                 <button onClick={() => setShowingNavigationDropdown(! showingNavigationDropdown)} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                                     <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                        <path className={{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                                        <path className={{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                                        <path className={classNames({'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown})} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                                        <path className={classNames({'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown})} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                                     </svg>
                                 </button>
                             </div>
