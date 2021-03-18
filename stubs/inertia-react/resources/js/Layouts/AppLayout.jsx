e7de66525fae62c69796005cd5e5f98a67c4a283
Roj Vroemen <me@rojvroemen.com>
2021-03-18 17:37:55 +0100

    Convert comments

--- stubs/inertia-react/resources/js/Layouts/AppLayout.jsx
+++ stubs/inertia-react/resources/js/Layouts/AppLayout.jsx
@@ -143,7 +143,7 @@ export default function AppLayout({ header, children}) {
 
                                         <div className="border-t border-gray-100"></div>
 
-                                        // Authentication
+                                        {/*Authentication*/}
                                         <form onSubmit={() => logout()}>
                                             <JetDropdownLink as="button">
                                                 Log Out
@@ -153,7 +153,7 @@ export default function AppLayout({ header, children}) {
                                 </div>
                             </div>
 
-                            // Hamburger
+                            {/*Hamburger*/}
                             <div className="-mr-2 flex items-center sm:hidden">
                                 <button onClick={() => setShowingNavigationDropdown(! showingNavigationDropdown)} className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                                     <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
